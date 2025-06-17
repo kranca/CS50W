87dd569d4dba872445a68c3d27e0925d406ffc6a
kranca <90696292+kranca@users.noreply.github.com>
2025-06-17 12:07:07 +0200

    Added watchers horizontal filter to listing in admin page
    - Added watchers horizontal filter to listing in admin page
    - Horizontal filter excludes owner of the listing

--- Project 2/auctions/admin.py
+++ Project 2/auctions/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
+from django import forms
 
 from .models import *
 
 # Register your models here.
+class ListingAdminForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(ListingAdminForm, self).__init__(*args, **kwargs)
+
+        if self.instance and self.instance.pk:
+            # Exclude the owner from watchers queryset
+            self.fields['watchers'].queryset = User.objects.exclude(pk=self.instance.owner_id)
+
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "username", "last_name", "first_name")
 
 class ListingAdmin(admin.ModelAdmin):
+    form = ListingAdminForm
     list_display = ("id", "object_name", "category", "is_active")
+    filter_horizontal = ("watchers",)
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ("id", "listing", "user", "offer")
